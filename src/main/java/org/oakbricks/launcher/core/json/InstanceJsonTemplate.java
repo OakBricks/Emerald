30e6b1eb68215cf600d523713695371e8009d840
OakBricks <andycruzfranco@gmail.com>
2022-01-01 16:21:29 -0600

    NonNull 😍

--- src/main/java/org/oakbricks/launcher/core/json/InstanceJsonTemplate.java
+++ src/main/java/org/oakbricks/launcher/core/json/InstanceJsonTemplate.java
@@ -1,9 +1,30 @@
 package org.oakbricks.launcher.core.json;
 
+import com.google.gson.Gson;
+import lombok.NonNull;
+
 import java.util.UUID;
 
 public class InstanceJsonTemplate {
-    UUID instanceUuid;
-    String instanceName;
 
+    public InstanceJsonTemplate(@NonNull UUID uuid, @NonNull String name, @NonNull String launchActionType, @NonNull String launchAction, @NonNull String exitActionType, @NonNull String exitAction) {
+        instanceUuid = uuid;
+        instanceName = name;
+        instanceLaunchActionType = launchActionType;
+        instanceLaunchAction = launchAction;
+        instanceExitActionType = exitActionType;
+        instanceExitAction = exitAction;
+    }
+    @NonNull
+    UUID instanceUuid;
+    @NonNull
+    String instanceName;
+    @NonNull
+    String instanceLaunchActionType;
+    @NonNull
+    String instanceLaunchAction;
+    @NonNull
+    String instanceExitActionType;
+    @NonNull
+    String instanceExitAction;
 }
