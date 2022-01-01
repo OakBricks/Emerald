bfdce7670840f6855ff77a2fde2401288d04b500
OakBricks <andycruzfranco@gmail.com>
2022-01-01 12:05:37 -0600

    launcher settings prep

--- src/main/java/org/oakbricks/launcher/core/json/LauncherConfigTemplate.java
+++ src/main/java/org/oakbricks/launcher/core/json/LauncherConfigTemplate.java
@@ -3,5 +3,6 @@ package org.oakbricks.launcher.core.json;
 public class LauncherConfigTemplate {
     int[] winPos;
     String windowIconPath;
-    
+    String uniqueLauncherUUID;
+    String launcherTheme;
 }
