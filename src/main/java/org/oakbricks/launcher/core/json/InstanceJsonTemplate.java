c95057705eeff29d0918436da0e6bfddd5bc08bf
OakBricks <andycruzfranco@gmail.com>
2021-12-31 18:02:29 -0600

    json foreshadowing

--- src/main/java/org/oakbricks/launcher/core/json/InstanceJsonTemplate.java
+++ src/main/java/org/oakbricks/launcher/core/json/InstanceJsonTemplate.java
@@ -1,5 +1,9 @@
 package org.oakbricks.launcher.core.json;
 
+import java.util.UUID;
+
 public class InstanceJsonTemplate {
+    UUID instanceUuid;
+    String instanceName;
 
 }
---
+++ src/main/java/org/oakbricks/launcher/core/json/LauncherConfigTemplate.java
@@ -0,0 +1,7 @@
+package org.oakbricks.launcher.core.json;
+
+public class LauncherConfigTemplate {
+    int[] winPos;
+    String windowIconPath;
+    
+}
