4eb27213fd295778e759480eae4bcfc20020e979
OakBricks <andycruzfranco@gmail.com>
2022-01-01 15:27:06 -0600

    launcher settings prep (part 2)

--- src/main/java/org/oakbricks/launcher/core/json/LauncherConfigTemplate.java
+++ src/main/java/org/oakbricks/launcher/core/json/LauncherConfigTemplate.java
@@ -1,8 +1,29 @@
 package org.oakbricks.launcher.core.json;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+
 public class LauncherConfigTemplate {
+    @Getter
+    @Setter
     int[] winPos;
+    @Getter
+    @Setter
     String windowIconPath;
+    @Getter
+    @Setter
     String uniqueLauncherUUID;
+    @Getter
+    @Setter
     String launcherTheme;
+    @Getter
+    @Setter
+    String lastUsedAccount;
+
+    @Override
+    public String toString() {
+        return "LauncherConfig(windowPosition: "+winPos+" windowIconPath: "+windowIconPath+" uniqueLauncherUUID: "+uniqueLauncherUUID+" launcherTheme: "+launcherTheme+" lastUsedAccount: "+lastUsedAccount+")";
+    }
 }
