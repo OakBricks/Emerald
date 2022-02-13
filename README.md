510b40752b13f222e4801aa8385544689e4e1b1b
OakBricks <andycruzfranco@gmail.com>
2022-02-13 11:29:18 -0600

    updates

---
+++ README.md
@@ -0,0 +1,13 @@
+# OakLauncher
+###### *Oh shit, here we go again*
+
+## Why?
+The original launcher (As seen in `master`) was getting too hard to maintain,
+so I redid the launcher now in QtJambi and with as little dependencies as possible.
+
+## Features checklist
+- [ ] Launching
+- [ ] Auth
+- [ ] Profiles
+- [ ] Settings
+- [x] GUI
--- launcher/src/main/java/org/oakbricks/launcher/InstancesWindow.java
+++ launcher/src/main/java/org/oakbricks/launcher/InstancesWindow.java
@@ -1,16 +1,34 @@
 package org.oakbricks.launcher;
 
-import io.qt.widgets.QLineEdit;
-import io.qt.widgets.QTabWidget;
-import io.qt.widgets.QWidget;
+import io.qt.core.QRect;
+import io.qt.core.Qt;
+import io.qt.widgets.*;
+
+import javax.swing.*;
 
 public class InstancesWindow extends QWidget {
     public InstancesWindow() {
-        setFixedSize(400, 400);
+//        setBaseSize(400, 400);
+        setMinimumSize(400, 400);
+//        setLayout(new QVBoxLayout());
+        QGridLayout layout = new QGridLayout(this);
+        QLayout cancelAndCreateButtonLayout = new QHBoxLayout();
 
-        QTabWidget tabWidget = new QTabWidget(this);
-        tabWidget.setFixedSize(300, 300);
+        QTabWidget tabWidget = new QTabWidget();
+//        tabWidget.setGeometry(10, 10, 350, 300);
         tabWidget.addTab(new VanillaNewInstanceWidget(), "Vanilla");
+
+        QPushButton createInstanceButton = new QPushButton("Create");
+//        createInstanceButton.setGeometry(310, 310, 50, 25);
+
+        QPushButton cancelButton = new QPushButton("Cancel");
+
+        layout.addWidget(tabWidget, 0, 0);
+//        layout.addWidget(createInstanceButton, 1, 0);
+//        layout.addWidget(cancelButton, 1, 1);
+        cancelAndCreateButtonLayout.addWidget(cancelButton);
+        cancelAndCreateButtonLayout.addWidget(createInstanceButton);
+        layout.addLayout(cancelAndCreateButtonLayout, 1, 0);
 
         setWindowTitle("Penis 2");
     }
