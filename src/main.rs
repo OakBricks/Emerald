577f78139d1447ce84f80939bde2298f61469c90
OakBricks <andycruzfranco@gmail.com>
2022-02-15 16:08:02 -0600

    init 2

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use iui::controls::Button;
+use iui::prelude::{Window, WindowType};
+use iui::UI;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hol' up, checking for updates.");
+
+    // UI init
+    let ui = UI::init().unwrap();
+
+    // Widgets init
+    let mut button = Button::new(&ui, "Ok");
+
+    // Uhhhh idk
+    let mut grid = LayoutGird::new(&ui);
+    grid.set_padded(&ui, true);
+    let mut window = Window::new(&ui, "OakLauncher Updater", 640, 480, WindowType::HasMenubar);
+    window.set_child(&ui, grid);
+    window.show(&ui);
+
+    ui.main();
 }
