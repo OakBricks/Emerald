8ddafe80f426a1d315a6cbc8a609f492232a8604
OakBricks <andycruzfranco@gmail.com>
2022-03-02 07:11:48 -0600

    create login stubs

--- api/src/main/java/org/oakbricks/launcherapi/auth/MicrosoftAuth.java
+++ api/src/main/java/org/oakbricks/launcherapi/auth/MicrosoftAuth.java
@@ -2,4 +2,12 @@ package org.oakbricks.launcherapi.auth;
 
 public class MicrosoftAuth {
 
+    /**
+     * Currently a stub in api v0
+     * @param email
+     * @param password
+     */
+    public static void loginUsingEmailAndPassword(String email, String password) {
+
+    }
 }
