2a32cffe9d3e1c2f93722a7a3b3ed170ce25af5f
OakBricks <andycruzfranco@gmail.com>
2022-06-26 06:20:41 -0500

    (API) [BF] oops forgot to uncomment

--- api/src/main/resources/msauthredir/index.js
+++ api/src/main/resources/msauthredir/index.js
@@ -1,27 +1,26 @@
-// var interval = 15 * 1000; // ms
-// var expectedTime = Date.now() + interval;
-// var timeRemaining = interval / 1000
-// var intervalID
+ var interval = 15 * 1000; // ms
+ var expectedTime = Date.now() + interval;
+ var timeRemaining = interval / 1000
+ var intervalID
 
-// function updateTimer() {
-//    timeRemaining = timeRemaining - 1;
-// //   console.log(timeRemaining);
-//    document.getElementById("windowCloseCountdown").innerHTML = `Window will close in ${timeRemaining} seconds`;
-//    if (timeRemaining < 1) {
-//       clearInterval(intervalID);
-//    }
-// }
-// intervalID = setInterval(updateTimer, 1000);
+ function updateTimer() {
+    timeRemaining = timeRemaining - 1;
+    document.getElementById("windowCloseCountdown").innerHTML = `Window will close in ${timeRemaining} seconds`;
+    if (timeRemaining < 1) {
+       clearInterval(intervalID);
+    }
+ }
+ intervalID = setInterval(updateTimer, 1000);
 
 
-// setTimeout(step, interval);
-// function step() {
-//     var nowSubByExpectedTime = Date.now() - expectedTime;
+ setTimeout(step, interval);
+ function step() {
+     var nowSubByExpectedTime = Date.now() - expectedTime;
 
-//     if (nowSubByExpectedTime > interval) {
-//         window.close();
-//     }
-//     window.close();
-//     expectedTime += interval;
-//     setTimeout(step, Math.max(0, interval - nowSubByExpectedTime));
-// }
+     if (nowSubByExpectedTime > interval) {
+         window.close();
+     }
+     window.close();
+     expectedTime += interval;
+     setTimeout(step, Math.max(0, interval - nowSubByExpectedTime));
+ }
